47503735de0171ac0913b1bb24b2687f2f983fa5
Jose Pascual <josepascual92@gmail.com>
2026-01-27 12:45:59 +0100

    Add vending machine factory

--- src/VendingMachine/Infrastructure/VendingMachineFactory.php
+++ src/VendingMachine/Infrastructure/VendingMachineFactory.php
@@ -2,7 +2,15 @@
 
 namespace App\VendingMachine\Infrastructure;
 
-class VendingMachineFactory
-{
+use App\VendingMachine\Domain\Model\VendingMachine;
+use App\VendingMachine\Domain\Service\ChangeMaker;
 
+final class VendingMachineFactory
+{
+    public function __construct(private ChangeMaker $changeMaker) {}
+
+    public function create(): VendingMachine
+    {
+        return VendingMachine::withDefaults($this->changeMaker);
+    }
 }
